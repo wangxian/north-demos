a182d458cb064c58e6d49e3d01f3184819166024
wangxian <xian366@126.com>
2022-09-06 19:39:23 +0800

    new project north demos

---
+++ north-webapp/src/main/java/top/xiqiu/test/WebTester.java
@@ -0,0 +1,9 @@
+package top.xiqiu.test;
+
+import top.xiqiu.north.North;
+
+public class WebTester {
+    public static void main(String[] args) {
+        North.start(WebTester.class);
+    }
+}
---
+++ north-webapp/src/main/java/top/xiqiu/test/entity/Login.java
@@ -0,0 +1,8 @@
+package top.xiqiu.test.entity;
+
+public class Login {
+
+    public String email;
+    public String password;
+
+}
