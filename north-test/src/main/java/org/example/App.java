f523e41538816eafd7784d2e12d36b05786ba44e
wangxian <xian366@126.com>
2022-09-12 23:17:30 +0800

    修改：改为多模块项目

---
+++ north-test/src/main/java/org/example/App.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import top.xiqiu.north.North;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        North.start(App.class);
+    }
+}
